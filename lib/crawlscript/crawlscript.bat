525f574fb23b27b684989bc97b5f481c0227527a
key0058 <thekey0058@163.com>
2015-07-19 06:22:32 +0800

    delete crawlscript

--- lib/crawlscript/crawlscript.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-java -cp "bin/tea.jar;lib/*;lib/plugins/*;%classpath%" org.tea.main.MyShell %1
--- lib/crawlscript/crawlscript.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp "bin/tea.jar:lib/*:lib/plugins/*:$classpath" org.tea.main.MyShell "$@"
